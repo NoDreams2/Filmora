d76068f1349592b98f2d81d813da9568e787ad1b
NoDreams <lykyanov2005@mail.ru>
2025-02-13 19:27:11 +0700

    finished the menu

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { createRoot } from 'react-dom/client';
 
 import App from './components/App.jsx';
 
+import './main.scss';
+
 createRoot(document.getElementById('root')).render(
   <>
     <CssBaseline />
